1199dc91b62393ec82f0ebbf4028cc2bcca45421
HDYOU <32186506+HDYOU@users.noreply.github.com>
2025-12-23 15:05:50 +0800

    fix bug

--- lib/core/site/pandatv/pandatv_danmaku.dart
+++ lib/core/site/pandatv/pandatv_danmaku.dart
@@ -11,7 +11,7 @@ class PandaTvDanmaku implements LiveDanmaku {
   WebScoketUtils? webScoketUtils;
 
   @override
-  int heartbeatTime = 40 * 1000; //默认是40s
+  int heartbeatTime = 20 * 1000; //默认是60s
 
   var serverUrl = "wss://chat-ws.neolive.kr/connection/websocket";
 
